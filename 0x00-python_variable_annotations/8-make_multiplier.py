5bdd08d2226f5372ba113075aec2cee95696260b
Ayobami <ayobamidele006@gmail.com>
2023-08-04 01:09:06 +0100

    make all files executable

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -13,4 +13,14 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     Returns:
         Callable[[float], float]: function that multiplies a float by multiplier
     """
-    return lambda x: x * multiplier
+    def multiply(n: float) -> float:
+        """Returns the product of multiplier and n.
+
+        Args:
+            n (float): float
+
+        Returns:
+            float: product of multiplier and n
+        """
+        return n * multiplier
+    return multiply
