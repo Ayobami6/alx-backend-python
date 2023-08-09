68280d683ca1f0c7999139ec385341d9ae63dee4
Ayobami <ayobamidele006@gmail.com>
2023-08-09 08:44:31 +0100

    refactor code to use for loop

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,8 +8,6 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator:
     """async_generator function """
-    i = 0
-    while i < 10:
+    for i in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
-        i += 1
+        yield random.uniform(0, 10)
