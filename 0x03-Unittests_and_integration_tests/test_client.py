3dc161e20bd885465c3d27a902e391c85e09ea20
Ayobami <ayobamidele006@gmail.com>
2023-08-28 11:22:51 +0100

    add test for public repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """ Test GithubOrgClient module """
 
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized, parameterized_class
 from utils import get_json, memoize
@@ -24,6 +24,21 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org)
         self.assertEqual(client.org, expected)
         mck_mtd.assert_called_once()
+
+    test_cases_2 = [
+        ("google", {"repos_url": "http://test_url"}),
+    ]
+
+    @parameterized.expand(test_cases_2)
+    def test_public_repos_url(self, org: str, expected: dict) -> None:
+        """ Test GithubOrgClient._public_repos_url """
+
+        # mock the org property to return the expected payload
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mck_prop:
+            mck_prop.return_value = expected
+            client = GithubOrgClient(org)
+            self.assertEqual(client._public_repos_url, expected["repos_url"])
 
 
 if __name__ == '__main__':
