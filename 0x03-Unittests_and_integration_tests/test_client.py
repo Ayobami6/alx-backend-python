bf04338849b58f47ce9d490290dc8fed8bf36c1d
Ayobami <ayobamidele006@gmail.com>
2023-08-28 12:27:50 +0100

    add test public repo

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -40,6 +40,34 @@ class TestGithubOrgClient(unittest.TestCase):
             client = GithubOrgClient(org)
             self.assertEqual(client._public_repos_url, expected["repos_url"])
 
+    test_cases_3 = [
+        ("google",
+         {"repos_url": "http://test_url",
+          "repos": [
+              {"name": "test_repo",
+               "license": {"key": "my_license"}},
+              {"name": "test_repo_2", "license": {
+                  "key": "other_license"}},
+          ]}),
+    ]
+
+    @parameterized.expand(test_cases_3)
+    @patch('client.get_json')  # patch get_json to not make external calls
+    def test_public_repos(
+            self, org: str, expected: dict, mck_mtd: Mock) -> None:
+        """ Test GithubOrgClient.public_repos """
+
+        #  make the return of simulated call to get_json return the expected
+        mck_mtd.return_value = expected["repos"]
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mck_prop:
+            mck_prop.return_value = expected.get("repos_url")
+            client = GithubOrgClient(org)
+            self.assertEqual(client.public_repos(), [
+                             "test_repo", "test_repo_2"])
+            mck_prop.assert_called_once()
+            mck_mtd.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
